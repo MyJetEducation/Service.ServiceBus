e83d416ec9593aa56f0f0d49c4b1281abd9d8656
dmitriy.b <dmitriy.b@smplt.net>
2022-01-28 15:23:04 +0500

    Extensions added

---
+++ src/Service.ServiceBus/Services/ServiceBusClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Autofac;
+using Microsoft.Extensions.Logging;
+using MyJetWallet.Sdk.ServiceBus;
+using MyServiceBus.TcpClient;
+
+namespace Service.ServiceBus.Services
+{
+	public static class ServiceBusClient
+	{
+		public static MyServiceBusTcpClient RegisterServiceBusClient<TServiceBusModel>(this ContainerBuilder builder, Func<string> getHostPort, string topicName, ILoggerFactory loggerFactory)
+			where TServiceBusModel : class
+		{
+			MyServiceBusTcpClient tcpServiceBus = builder.RegisterMyServiceBusTcpClient(getHostPort, loggerFactory);
+
+			builder
+				.RegisterInstance(new MyServiceBusPublisher<TServiceBusModel>(tcpServiceBus, topicName, false))
+				.As<IServiceBusPublisher<TServiceBusModel>>()
+				.SingleInstance();
+
+			tcpServiceBus.Start();
+
+			return tcpServiceBus;
+		}
+		
+		public static MyServiceBusTcpClient RegisterServiceBusClient(this ContainerBuilder builder, Func<string> getHostPort, ILoggerFactory loggerFactory)
+		{
+			return builder.RegisterMyServiceBusTcpClient(getHostPort, loggerFactory);
+		}
+	}
+}
