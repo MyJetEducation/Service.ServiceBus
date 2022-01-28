f1ffa0a742d8e0c4fe284915f67cbd2fcd11a107
dmitriy.b <dmitriy.b@smplt.net>
2022-01-28 15:35:14 +0500

    Add subscriber extensions

--- src/Service.ServiceBus/Services/ServiceBusClient.cs
+++ src/Service.ServiceBus/Services/ServiceBusClient.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using Autofac;
+using DotNetCoreDecorators;
 using Microsoft.Extensions.Logging;
 using MyJetWallet.Sdk.ServiceBus;
+using MyServiceBus.Abstractions;
 using MyServiceBus.TcpClient;
 
 namespace Service.ServiceBus.Services
@@ -22,10 +25,30 @@ namespace Service.ServiceBus.Services
 
 			return tcpServiceBus;
 		}
-		
+
 		public static MyServiceBusTcpClient RegisterServiceBusClient(this ContainerBuilder builder, Func<string> getHostPort, ILoggerFactory loggerFactory)
 		{
 			return builder.RegisterMyServiceBusTcpClient(getHostPort, loggerFactory);
+		}
+
+		public static ContainerBuilder RegisterServiceBusSubscriberBatch<T>(this ContainerBuilder builder, MyServiceBusTcpClient client, string topicName, string queueName, TopicQueueType queryType = TopicQueueType.Permanent)
+		{
+			builder
+				.RegisterInstance(new MyServiceBusSubscriber<T>(client, topicName, queueName, queryType, true))
+				.As<ISubscriber<IReadOnlyList<T>>>()
+				.SingleInstance();
+
+			return builder;
+		}
+
+		public static ContainerBuilder RegisterServiceBusSubscriberSingle<T>(this ContainerBuilder builder, MyServiceBusTcpClient client, string topicName, string queueName, TopicQueueType queryType = TopicQueueType.Permanent)
+		{
+			builder
+				.RegisterInstance(new MyServiceBusSubscriber<T>(client, topicName, queueName, queryType, false))
+				.As<ISubscriber<T>>()
+				.SingleInstance();
+
+			return builder;
 		}
 	}
 }
