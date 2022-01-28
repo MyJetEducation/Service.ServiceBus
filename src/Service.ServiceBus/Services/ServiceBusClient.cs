b36505bb0a03f6c05407d5c213b0dce18783e649
dmitriy.b <dmitriy.b@smplt.net>
2022-01-28 17:12:44 +0500

    Fix client

--- src/Service.ServiceBus/Services/ServiceBusClient.cs
+++ src/Service.ServiceBus/Services/ServiceBusClient.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Autofac;
 using DotNetCoreDecorators;
 using Microsoft.Extensions.Logging;
-using MyJetWallet.Sdk.Service.LivnesProbs;
 using MyJetWallet.Sdk.ServiceBus;
 using MyServiceBus.Abstractions;
 using MyServiceBus.TcpClient;
@@ -14,29 +13,7 @@ namespace Service.ServiceBus.Services
 	{
 		public static MyServiceBusTcpClient RegisterServiceBusClient(this ContainerBuilder builder, Func<string> getHostPort, ILoggerFactory loggerFactory)
 		{
-			MyServiceBusTcpClient serviceBusClient = builder.RegisterMyServiceBusTcpClient(getHostPort, loggerFactory);
-
-			var manager = new ServiceBusManager(serviceBusClient, getHostPort?.Invoke());
-			builder.RegisterInstance(manager).As<IServiceBusManager>().SingleInstance();
-
-			builder.RegisterType<ServiceBusLifeTime>().AsSelf().As<ILivenessReporter>().SingleInstance().AutoActivate();
-
-			return serviceBusClient;
-		}
-
-		public static MyServiceBusTcpClient RegisterServiceBusClient<TServiceBusModel>(this ContainerBuilder builder, Func<string> getHostPort, string topicName, ILoggerFactory loggerFactory)
-			where TServiceBusModel : class
-		{
-			MyServiceBusTcpClient tcpServiceBus = builder.RegisterServiceBusClient(getHostPort, loggerFactory);
-
-			builder
-				.RegisterInstance(new ServiceBusPublisher<TServiceBusModel>(tcpServiceBus, topicName, false))
-				.As<IPublisher<TServiceBusModel>>()
-				.SingleInstance();
-
-			tcpServiceBus.Start();
-
-			return tcpServiceBus;
+			return builder.RegisterMyServiceBusTcpClient(getHostPort, loggerFactory);
 		}
 
 		public static ContainerBuilder RegisterServiceBusSubscriberBatch<T>(this ContainerBuilder builder, MyServiceBusTcpClient client, string topicName, string queueName, TopicQueueType queryType = TopicQueueType.Permanent)
