9d05ded3baafa139b8df856b8c44f2215c7deccb
dmitriy.b <dmitriy.b@smplt.net>
2022-03-16 14:02:20 +0500

    FIN360PMVP-238: New bus model

---
+++ src/Service.ServiceBus/Models/UserTimeChangedServiceBusModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Service.ServiceBus.Models
+{
+	/// <summary>
+	///     Новое значение пребывание пользователя в сервисе в течение определенного времени
+	/// </summary>
+	[DataContract]
+	public class UserTimeChangedServiceBusModel
+	{
+		public const string TopicName = "myjeteducation-usertime";
+
+		[DataMember(Order = 1)]
+		public Guid? UserId { get; set; }
+
+		[DataMember(Order = 2)]
+		public TimeSpan TotalSpan { get; set; }
+
+		[DataMember(Order = 3)]
+		public TimeSpan TodaySpan { get; set; }
+	}
+}
--- test/Service.ServiceBus.Tests/ServiceBusModelTests.cs
+++ test/Service.ServiceBus.Tests/ServiceBusModelTests.cs
@@ -20,7 +20,8 @@ namespace Service.ServiceBus.Tests
 			(typeof (UserAccountFilledServiceBusModel), new UserAccountFilledServiceBusModel {UserId = new Guid("7568c9bb-438a-4d80-ab84-d8d6763d7f7f")}),
 			(typeof (UserProgressUpdatedServiceBusModel), new UserProgressUpdatedServiceBusModel {UserId = new Guid("7568c9bb-438a-4d80-ab84-d8d6763d7f7f"), HabitCount = 1}),
 			(typeof (ChangeEmailServiceBusModel), new ChangeEmailServiceBusModel {Email = "some@email.com", Hash = "123"}),
-			(typeof (NewPaymentServiceBusModel), new NewPaymentServiceBusModel {CardId = new Guid("44584892-a988-43a7-98cd-57654894df8e"), TransactionId = new Guid("3ba27c2f-c503-4960-a3e2-2ca2f5b1cefc"), UserId = new Guid("08c6f0ac-2a1b-4970-b0a2-17d5c945a293")})
+			(typeof (NewPaymentServiceBusModel), new NewPaymentServiceBusModel {CardId = new Guid("44584892-a988-43a7-98cd-57654894df8e"), TransactionId = new Guid("3ba27c2f-c503-4960-a3e2-2ca2f5b1cefc"), UserId = new Guid("08c6f0ac-2a1b-4970-b0a2-17d5c945a293")}),
+			(typeof (UserTimeChangedServiceBusModel), new UserTimeChangedServiceBusModel {UserId = new Guid("08c6f0ac-2a1b-4970-b0a2-17d5c945a293"), TotalSpan = TimeSpan.FromDays(1), TodaySpan = TimeSpan.FromMinutes(1)})
 		};
 
 		public static void AreEqualByJson(object expected, object actual)
