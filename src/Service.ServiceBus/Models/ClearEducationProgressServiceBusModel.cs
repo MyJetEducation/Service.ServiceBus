f909c746c9fd83b69d33057b82fa8e470eacab01
dmitriy.b <dmitriy.b@smplt.net>
2022-04-13 17:03:02 +0500

    Split clear edu progress models#fixed

--- src/Service.ServiceBus/Models/ClearEducationProgressServiceBusModel.cs
+++ src/Service.ServiceBus/Models/ClearEducationProgressServiceBusModel.cs
@@ -15,12 +15,33 @@ namespace Service.ServiceBus.Models
 		public string UserId { get; set; }
 
 		[DataMember(Order = 2)]
-		public EducationTutorial? Tutorial { get; set; }
+		public EducationTutorial? ClearTutorial { get; set; }
 
 		[DataMember(Order = 3)]
-		public int? Unit { get; set; }
+		public int? ClearUnit { get; set; }
 
 		[DataMember(Order = 4)]
-		public int? Task { get; set; }
+		public int? ClearTask { get; set; }
+
+		[DataMember(Order = 5)]
+		public bool ClearAchievements { get; set; }
+
+		[DataMember(Order = 6)]
+		public bool ClearStatuses { get; set; }
+
+		[DataMember(Order = 7)]
+		public bool ClearHabits { get; set; }
+
+		[DataMember(Order = 8)]
+		public bool ClearSkills { get; set; }
+
+		[DataMember(Order = 9)]
+		public bool ClearKnowledge { get; set; }
+
+		[DataMember(Order = 10)]
+		public bool ClearUserTime { get; set; }
+
+		[DataMember(Order = 11)]
+		public bool ClearRetry { get; set; }
 	}
-} 
+}
