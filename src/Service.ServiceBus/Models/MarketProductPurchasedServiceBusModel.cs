d0763fe8a28c7bbca3d73250dd941d61475809a9
dmitriy.b <dmitriy.b@smplt.net>
2022-04-18 11:47:12 +0500

    Change sbus model

---
+++ src/Service.ServiceBus/Models/MarketProductPurchasedServiceBusModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+using Service.MarketProduct.Domain.Models;
+
+namespace Service.ServiceBus.Models
+{
+	/// <summary>
+	///     Пользователь купил товар
+	/// </summary>
+	[DataContract]
+	public class MarketProductPurchasedServiceBusModel
+	{
+		public const string TopicName = "myjeteducation-market-product-purchased";
+
+		[DataMember(Order = 1)]
+		public string UserId { get; set; }
+
+		[DataMember(Order = 2)]
+		public MarketProductType Product { get; set; }
+
+		[DataMember(Order = 3)]
+		public decimal? Amount { get; set; }
+	}
+}
